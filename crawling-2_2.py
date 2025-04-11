d4975498cb14a149eac4c0ec7f66f4fc198a7258
yis <yis1149@gmail.com>
2025-04-11 17:57:00 +0900

    구글 보안창 처리를 위한 테스트 crawling-2_2.py

--- crawling-2_2.py
+++ crawling-2_2.py
@@ -59,12 +59,12 @@ stealth(driver,
         fix_hairline=True
         )
 
-options.add_argument('--remote-debugging-port=9222')
+#options.add_argument('--remote-debugging-port=9222')
 
 
 
 # 크롬드라이버 url 주소 넣고 실행
-driver.get('https://www.google.co.kr')
+driver.get('https://www.google.com')
 
 # 페이지가 완전히 로딩되도록 3초동안 기다림
 time.sleep(5)
@@ -73,7 +73,7 @@ time.sleep(5)
 search_box = driver.find_element(By.CLASS_NAME, "gLFyf")		
 
 # 검색 버튼을 찾아 search 변수에 저장
-#search_button = driver.find_element(By.CLASS_NAME, "gNO89b")	
+search_button = driver.find_element(By.CLASS_NAME, "gNO89b")	
 #search_button = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]')	
 
 # 검색어 창을 찾아 search 변수에 저장 (By.XPATH 방식) (※ html 코드 위에서 마우스 우클릭-> copy -> Copy Xpath)
@@ -82,8 +82,7 @@ search_box = driver.find_element(By.CLASS_NAME, "gLFyf")
 
 # 검색어 입력창에 '파이썬' 검색어 입력후 '엔터키' 입력
 search_box.send_keys('파이썬')	
-time.sleep(1)
-search_box.send_keys(Keys.RETURN)
+search_button.click()
 
 # click 함수는 ()안에 아무것도 넣지 않으면 좌클릭을 수행
 #search_button.click()
