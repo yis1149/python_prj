0c03b995977a43ff9dab9e369407e9e6c1bc67fb
LAPTOP-5Q0I5VUB\DongYoung <yis1149@gmail.com>
2025-04-17 22:54:12 +0900

    google 뉴스 크롤링 오류 수정

--- crawling training/lib_BeautifullSoups-1_1.py
+++ crawling training/lib_BeautifullSoups-1_1.py
@@ -10,7 +10,7 @@ from newspaper import Article
 from selenium import webdriver
 
 
-keyword = '아마존닷컴'
+keyword = '파이썬 기초'
 page_num = 0 #0, 10,20...
 news_df = pd.DataFrame()
 
@@ -44,21 +44,60 @@ soup = BeautifulSoup(content, 'html.parser')
 # last page check
 #if soup.select('div.ilUpNd.UFvD1.aSRlid') == []: break
 
-title_lsit = [t.text for t in soup.select('div.ilUpNd.UFvD1.aSRlid')] #title
+title_list = [t.text for t in soup.select('div.ilUpNd.UFvD1.aSRlid')] #title
 
 url_list = []
 
-print(soup.prettify())
-print(soup.select('div.ilUpNd.UFvD1.aSRlid'))
-print(title_lsit)
+#print(soup.prettify())
+#print(soup.select('div.ilUpNd.UFvD1.aSRlid'))
+#print(title_lsit)
+#print(url)
 
 
 # url
 for u in soup.select('a'):
-    for t in title_lsit:
-        print(t.text)
+    i = 0
+    i+= 1
+    for t in title_list:            
         if t in u.text:
-            #temp_url = urllib.parse.unquote(u['href'])
-            #temp_url = re.findall("http\S+",temp_url)[0][:-3]
-            #url_list.append(temp_url)
-            print(u.text)
+            temp_url = urllib.parse.unquote(u['href'])
+            temp_url = re.findall(r"http\S+",temp_url)[0][:-3]
+            url_list.append(temp_url)
+            print("t_" + str(i) + " : " + temp_url)
+
+print("title_list_len : " + str(len(title_list)))
+print("url_list_len : " + str(len(url_list)))
+
+# article
+for ind, news_url in enumerate(url_list):
+    try:
+        article = Article(url=news_url)
+        article.download()
+        article.parse()    
+        news_article = article.text
+    except: #ssl error
+        driver.get(news_url)
+        article.download(input_html=driver.page_source)
+        article.parse()
+        news_article = article.text
+
+'''
+
+    if ind < len(title_list):
+        news_df = pd.concat([news_df, pd.DataFrame([[title_list[ind], news_article, news_url]])])
+
+news_df[0] = news_df[0].apply(lambda x: re.sub(r'\s+',' ',x))
+news_df = news_df.reset_index(drop=True)
+
+#page_num += 10
+if not news_df.empty:
+    news_df.to_excel('news_crawling.xlsx', index=False)
+else:
+    print("news_df가 비어 있습니다. 데이터를 확인하세요.")
+
+news_df.columns = ['제목','본문','URL']
+
+'''
+
+# WebDriver 종료
+driver.quit()
